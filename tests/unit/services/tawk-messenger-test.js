03fc80a9339a49ec822896bca7033ecdc3610935
Jerald Austero <jerald.austero@tawk.to>
2022-05-13 13:16:19 +0800

    Add test for checking global variables of Tawk

--- tests/unit/services/tawk-messenger-test.js
+++ tests/unit/services/tawk-messenger-test.js
@@ -1,12 +1,18 @@
 import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
 
 module('Unit | Service | tawk-messenger', function (hooks) {
-	setupTest(hooks);
+	setupRenderingTest(hooks);
 
-	test('Should throw an error', function (assert) {
-		window.Tawk_API = {};
-		let service = this.owner.lookup('service:tawk-messenger');
-		assert.ok(service);
+	test('Should have global variables \'Tawk_API, Tawk_LoadStart\'', async function (assert) {
+		await render(hbs `<TawkMessenger />`);
+
+		const isTawkAPIObject = typeof window.Tawk_API === 'object';
+		const isTawkLoadStartDate = window.Tawk_LoadStart instanceof Date;
+
+		assert.ok(isTawkAPIObject, 'Tawk_API is not a object');
+		assert.ok(isTawkLoadStartDate, 'Tawk_LoadStart is not date');
 	});
 });
