d0d55c0e440f3c271789df19d5fcea3acb102f8a
Jerald Austero <jerald.austero@tawk.to>
2023-11-13 17:50:41 +0800

    Add only autoStart when disabled

--- addon/utils/widget.js
+++ addon/utils/widget.js
@@ -13,7 +13,7 @@ function loadScript({propertyId = '', widgetId = '', embedId = '', basePath = 't
 		window.Tawk_API.embedded = embedId;
 	}
 
-	if (autoStart) {
+	if (!autoStart) {
 		window.Tawk_API.autoStart = autoStart;
 	}
 
